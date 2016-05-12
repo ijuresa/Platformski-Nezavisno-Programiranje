d4204a66ededab4a7f0a6e022b2e2668dd4a8621
Ivan Juresa <ivanjuresa611@gmail.com>
2016-05-12 23:02:48 +0200

    First Commit

---
+++ PNP_Firmware.cpp
@@ -0,0 +1,13 @@
+// PNP_Firmware.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout << "WC Platformsko Nezavisno Programiranje :)" << endl;
+
+	return 0;
+}
+
