308177231f75f7cf97dfc74b35529cd6d0886c36
Ivan Juresa <ivanjuresa611@gmail.com>
2016-08-30 10:52:57 +0200

    Exported library

---
+++ newLibrary/build/actualLibrary/CMakeFiles/actualLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/actualLibrary.dir/actuallibrary.cpp.o"
+  "libactualLibrary.pdb"
+  "libactualLibrary.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/actualLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
